918c573f42cc2d30b5534238858d914a001d935d
riquedev <rique_dev@hotmail.com>
2022-07-02 23:35:13 -0300

    :bug: fix windows changes

--- public/src/scripts/install.cmd
+++ public/src/scripts/install.cmd
@@ -1,1 +1,4 @@
-git clone git@github.com:4U360/KoterSDK.git . && pip install -r requirements.txt &&  python public\src\installer.py
+git clone git@github.com:4U360/KoterSDK.git .
+pip install -r requirements.txt &&
+python public\src\installer.py
+echo "Sucess!"

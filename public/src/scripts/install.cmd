d59e74329dc2c0c092c80d4f1c679b8ce215714d
riquedev <rique_dev@hotmail.com>
2022-07-02 23:25:03 -0300

    :bug: fix scripts

--- public/src/scripts/install.cmd
+++ public/src/scripts/install.cmd
@@ -1,3 +1,3 @@
-curl https://download.koter.dev.br -O koter-sdk.zip
+curl https://github.com/4U360/KoterSDK/releases/download/test-release/installer.zip -O koter-sdk.zip
 tar -xf koter-sdk.zip
 pip install -r requirements.txt
